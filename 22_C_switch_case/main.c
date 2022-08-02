ac9c541d2e3a0ad07b27964959f63e31de4865d0
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-08-01 21:45:09 -0300

    Add files via upload

---
+++ 22_C_switch_case/main.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(){
+           int a = 5;
+           switch (a){
+                  case 1:
+                    printf("\n a e 1.\n");
+                    break;
+                  case 2:
+                    printf("\n a e 2.\n");
+                    break;
+                  case 3:
+                    printf("\n a e 3.\n");
+                    break;
+                  default:
+                    printf("\n opcao invalida.\n");
+                    break;
+
+                     }
+                     return 0;
+                     system("pause");
+
+
+
+          }
