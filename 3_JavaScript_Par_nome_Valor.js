ce3eb7bad3e1af2f00651ea819861e1be5a9914e
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-08-03 11:21:46 -0300

    Add files via upload

---
+++ 3_JavaScript_Par_nome_Valor.js
@@ -0,0 +1,20 @@
+const saudacao = 'opa'
+
+function exec(){
+    const saudacao ='faaala'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'fabio',
+    idade:39,
+    peso:80,
+    endereco: {
+        logradouro: 'rua a ',
+        numero: 123
+    }
+}
+
+console.log (saudacao)
+console.log(exec())
+console.log(cliente)
