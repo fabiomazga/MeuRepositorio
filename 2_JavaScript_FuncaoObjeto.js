e64c1c3e76a5aa018d3887e009d3d1115f4835d7
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-08-03 10:20:36 -0300

    Add files via upload

---
+++ 2_JavaScript_FuncaoObjeto.js
@@ -0,0 +1,9 @@
+console.log (typeof Object) // se trata de uma função 
+console.log (typeof new Object)  // se trata de um objeto
+const Cliente = function() {}
+console.log (typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // classe
+console.log(typeof Produto)
+console.log(typeof new Produto())
