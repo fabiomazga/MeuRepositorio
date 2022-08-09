57105584874f26604a63df38762cac18bce534bb
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-08-09 10:53:41 -0300

    Add files via upload

--- 7_JavaScript_This.js
+++ 7_JavaScript_This.js
@@ -1,4 +1,4 @@
-function Pessoa(){
+/*function Pessoa(){
    this.idade = 0;
    setInterval(() => {
     this.idade++;
@@ -7,3 +7,17 @@ function Pessoa(){
 
 }
 new Pessoa
+*/
+let comparaComThis = function (param){
+   console.log(this === param) // sem arrow aponta pro global
+}
+comparaComThis(global)
+const obj = {}
+comparaComThis (global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param) // com arrow o this não aponta pro global
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
+
