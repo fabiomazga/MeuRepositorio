b5cee794ea52734ddd7e7ae2859fbcbcb8aec01b
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-08-09 16:48:22 -0300

    Add files via upload

---
+++ 12_JavaScript_Array.js
@@ -0,0 +1,9 @@
+console.log (typeof Array, typeof new Array, typeof [])
+let aprovados = new Array ('fabio', 'pedro', 'laura')
+console.log(aprovados)
+aprovados = ['fabio', 'pedro', 'laura']
+console.log(aprovados[0])
+aprovados[3]='daniel'
+aprovados.push('fernanda')
+console.log(aprovados.length)
+console.log(aprovados[4])
