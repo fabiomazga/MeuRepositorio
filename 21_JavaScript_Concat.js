971f63e129ba57e15db995c70b48e784a80bcb8b
Fábio T.G <52431226+fabiomazga@users.noreply.github.com>
2022-08-16 13:59:34 -0300

    Add files via upload

---
+++ 21_JavaScript_Concat.js
@@ -0,0 +1,7 @@
+const filhos = ['pedro', 'guilherme']
+const filhas = ['laura', 'gabriela']
+const todos = filhos.concat(filhas)
+console.log(todos)
+
+console.log([].concat([1,2],[3,4],5,[[6,7]]))
+
